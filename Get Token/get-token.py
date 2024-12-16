16b4d0ee51ac995e871eb2366b01d6e3f7d3d1ac
Jhonny <82520698+Jhonny-Shi@users.noreply.github.com>
2024-12-16 20:05:02 +0800

    Update get-token.py

--- Get Token/get-token.py
+++ Get Token/get-token.py
@@ -60,7 +60,7 @@ class Service2Captcha:
 
     async def get_captcha_token_async(self):
         result = await asyncio.to_thread(
-            lambda: self.solver.solve_turnstile(
+            lambda: self.solver.request(
                 sitekey=CaptchaConfig.WEBSITE_KEY,
                 url=CaptchaConfig.WEBSITE_URL
             )
